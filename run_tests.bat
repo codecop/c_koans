1f40673a76d94af458a696b965477979a85a8cd9
Peter Kofler <peter.kofler@code-cop.org>
2020-03-03 20:43:10 +0100

    Script to run koans in kind of order.

---
+++ run_tests.bat
@@ -0,0 +1,53 @@
+@rem Run all tests of all sessions.
+@cls
+@color 02
+call make all
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_basics
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_strings
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_control_statements
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+@rem needs string
+bin\about_functions
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+@rem needs string
+bin\about_pointers
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+@rem needs strings, pointers
+bin\about_malloc
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_arrays
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_structs
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_dataclasses
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_linked_lists
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+@rem needs string
+bin\about_preprocessor
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+@rem needs string
+bin\about_printing
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+bin\about_io
+@if errorlevel 1 @color 04 && @echo FAILED && @goto end
+
+:end
+@pause
+run_tests.bat
